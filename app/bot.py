df14867c64d86dcc6cccc0952cf64ba62320f562
Maxim Pak <maxpak99@gmail.com>
2019-03-02 12:48:26 +0000

    Init 2.0

--- app/bot.py
+++ app/bot.py
@@ -2,10 +2,11 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from simple_settings import settings
 
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s\n%(message)s\n',
                     level=logging.INFO,
                     filename=settings.logfile)
+
+start_text = "Привет! Я тот, кто будет водить. Собери всех участников в чатик и пригласи меня туда. Когда все будут готовы просто напишите /start_quest"
 
 
 def logger(func):
@@ -22,21 +23,40 @@ def logger(func):
 @logger
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
-                     text="I'm a bot, please talk to me!")
+                     text='Привет, пригласи меня в чатик твоей команды...')
 
+
+next_response = 'Next Quest Started /answer'
+answer_response = 'Good job! /start'
+tip_response = 'Here is your tip /next'
 
 @logger
-def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+def next_quest(bot, update):
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=next_response)
 
+@logger
+def check_answer(bot, update):
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=answer_response)
+
+@logger
+def get_tip(bot, update):
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=tip_response)
 
 updater = Updater(settings.token)
 dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', start)
-echo_handler = MessageHandler(Filters.text, echo)
+answer_handler = CommandHandler('answer', check_answer)
+next_handler = CommandHandler('next', next_quest)
+tip_handler = CommandHandler('tip', get_tip)
 
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(answer_handler)
+dispatcher.add_handler(next_handler)
+dispatcher.add_handler(tip_handler)
 
 updater.start_polling()
+
